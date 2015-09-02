c1a5529c6c005043daf841290c8a4951bd95854b
Rohit Jain <rj.2929@yahoo.com>
2015-09-02 21:15:54 +0530

    hackathon changes

--- server_esp.js
+++ server_esp.js
@@ -1,5 +1,5 @@
 //io.sockets.in.... every socket in room
-//socketbroadcast.to ... every socket except itself
+//socket.broadcast.to ... every socket except itself
 (function(){
 	
 	var express = require('express');
@@ -8,17 +8,18 @@
 	var io = require('socket.io').listen(http);
 	var url = require('url');
 	var port = Number(process.env.PORT || 7000),
+  moment = require('moment'),
 	sys = require('sys'),
 	cors = require('cors'),
+  path = require('path'),
 	fs = require('fs');
-
 	var knex = require('knex')({
   		client: 'pg',
   		connection: {
-    		host     : YOUR_HOST,
-    		user     :USER,
-    		password : PASS,
-    		database : DB_NAME
+    		host     : 'localhost',
+    		user     :'postgres',
+    		password : 'rohit',
+    		database : 'ESP'
   		}
 	});
 	
@@ -34,7 +35,21 @@
   	   res.sendfile(__dirname+'/rules.html');
 	});
 
-  //init the database
+  app.get('/admin_panel',function(req,res){
+    res.sendfile(__dirname+'/admin_panel.html')
+  })
+  app.get('/stat_details',function(req,res){
+
+    res.json(global.round_wins)
+  })
+
+    app.get('/stat_points',function(req,res){
+
+    res.json(global.round_wins_points)
+  })
+
+
+  //init the database, Only One time call
   app.get('/init',function(req,res){
     knex.schema.createTable('game', function (table) {
        table.string('Name');
@@ -46,27 +61,18 @@
 
   });
 
-  //TO gte some db info
-	app.get('/info', function(req, res) {
+ app.get('/leader', function(req, res) {
+    res.sendfile(__dirname+'/leaderboard.html');
+  });
 
-	    knex('game').select().where({
-	        Name: 'game_1'
-	    }).exec(function(err, results) {
-	        if (err)
-	            res.send(err + " --->1");
-	        else{
-	            res.send(results[0]);
-	            console.log(results[0]['levels'])
-	        }
-	    });
-	});
-	
-  //Route to serve the game
-	app.get('/:game_name', function(req, res) {
+
+  app.get('/:game_name', function(req, res) {
     console.log('request for room: ' + req.params.game_name);
-    // if (req.params.stanza) {
-    //   urlRoom = req.params.stanza;
-    // }
+    res.send('Invalid Request');
+  });
+  //Route to serve the game
+	app.get('/:game_name/:uname', function(req, res) {
+    console.log('request for room: ' + req.params.game_name);
     res.sendfile(__dirname+'/game_template.html');
   });
 
@@ -121,48 +127,122 @@
     return a - b;
 	}
 
-  function createOrJoin(socket, room) {
-    var numClients = io.sockets.clients(room).length;
-
-    if (numClients == 0) {
-      socket.join(room);
-      socket.name ='O_1';
-      socket.emit('created', room,socket.name);
-    }
-    else if (numClients < 2 ) {
-      socket.join(room);
-      socket.name ='O_2';
-      socket.broadcast.to(room).emit('join', room);
-      socket.emit('joined', room,socket.name);
-  	}
+  function findOtherPlayer(socket){
+    if(socket.name_obj == global.room_users[socket.urlRoom][0])
+        return(global.room_users[socket.urlRoom][1]);
     else
-      	socket.emit('full', room);
-    }
+        return(global.room_users[socket.urlRoom][0]);
+  }
 
+  function createOrJoin(socket, room,uname) {
+    var numClients = io.sockets.clients(room).length;
+  
+        if (numClients == 0) {
+          socket.join(room);
+          global.room_users[room]=[]
+          global.room_users[room].push(uname+'_1')
+          socket.name_obj = uname+'_1';
+          socket.emit('created', room);
+        }
+        else if (numClients < 2 ) {
+          socket.join(room);
+          global.room_users[room].push(uname+'_2')
+          socket.name_obj = uname+'_2';
+          socket.broadcast.to(room).emit('join', room);
+          socket.emit('joined', room);
+      	}
+        else
+          	socket.emit('full', room);
+
+        return room;
+}
 
   global.responses = {}
-
+  global.room_users ={}
+  global.round_wins = {}
+  global.create_time = {}
+  global.answer_time ={}
+  global.round_win_number ={}
+  global.round_wins_points ={}
   io.sockets.on('connection', function(socket) {
 
   		//Called on creation of game _room
-  		socket.on('create or join', function(room) {
-  			 if(typeof(global.responses[room]) == 'undefined'){
-  			 	global.responses[room]={};
-  			 }
-     		 socket.urlRoom = room;
-     		 socket.cur_level = 1;
-      		 createOrJoin(socket, socket.urlRoom);
-      		 knex('game').select().where({
-		        Name: socket.urlRoom
+  		socket.on('create or join', function(room,uname) {
+
+        var numClients = io.sockets.clients(room).length;
+        socket.game_name = room;
+        socket.name = uname;
+        socket.time_answers = {}
+        socket.time_creations = {}
+
+
+    //If a game already has 2 opponents and a new opponent joins in,we create a room+_n for that new opponents
+        if(numClients ==2){
+            i=2
+            while(1){
+                    room_temp = room + '_room'+i;
+                    len = io.sockets.clients(room_temp).length;
+                    if(len<2){
+                      room = room_temp;
+                      break;
+                    }
+
+                    i+=1;
+                  }
+            }
+
+         if(typeof(global.responses[room]) == 'undefined'){
+          global.responses[room]={};
+         }
+
+         if(typeof(global.round_wins[room]) == 'undefined'){
+          global.round_wins[room]={};
+          global.round_win_number[room]={};
+          global.round_wins_points[room] = {}
+
+         }
+
+         if(typeof(global.create_time[room]) == 'undefined'){
+              global.create_time[room]={}
+              global.answer_time[room]={}
+
+         }
+     		
+        console.log('room: ' + room);
+
+        socket.cur_level = 1;
+        socket.cur_round = 1;
+      	socket.urlRoom = room;
+        createOrJoin(socket, room,socket.name);
+      	
+       
+
+        knex('game').select().where({
+		        Name: socket.game_name
 		     }).exec(function(err, results) {
 		        if (err)
 		            socket.emit('Recieve','db_err')
 		        else
-		            socket.levels = results[0]['levels'];
+		            socket.rounds = results[0]['levels'];
 		    });
-      		 global.responses[socket.urlRoom][socket.name]={}
-      		 global.responses[socket.urlRoom][socket.name][socket.cur_level]=[];
-      		 console.log(global.responses);
+      	
+        global.responses[socket.urlRoom][socket.name_obj]={}
+      	global.responses[socket.urlRoom][socket.name_obj][socket.cur_round+'_'+socket.cur_level]=[];
+        global.round_wins[room][socket.name_obj] =[]
+        global.round_wins_points[room][socket.name_obj] = 0 
+
+
+        socket.time_creations['Round_'+socket.cur_round] = []
+        socket.time_answers['Round_'+socket.cur_round] = []
+
+        
+        //Push this when other user joins, change!!
+        socket.time_creations['Round_'+socket.cur_round].push(moment())
+
+        global.create_time[socket.urlRoom][socket.name_obj] = socket.time_creations
+
+        global.round_win_number[room][socket.name_obj] = 0
+
     	});
 
   		//When you disconnect midway, abort the game 
@@ -174,58 +254,172 @@
   		//Called on Client behalf, when he presses any checkbox 
   		socket.on('RecieveClientMessage',function(message,operation){
   			if(operation=='push'){
-  				global.responses[socket.urlRoom][socket.name][socket.cur_level].push(message);
-  				global.responses[socket.urlRoom][socket.name][socket.cur_level].sort(compareNumbers);
+          socket.time_answers['Round_'+socket.cur_round][socket.cur_level-1] = moment()
+          global.answer_time[socket.urlRoom][socket.name_obj] = socket.time_answers
+  				
+          global.responses[socket.urlRoom][socket.name_obj][socket.cur_round+'_'+socket.cur_level].push(message);
+  				global.responses[socket.urlRoom][socket.name_obj][socket.cur_round+'_'+socket.cur_level].sort(compareNumbers);
   				}
   			else
-  				global.responses[socket.urlRoom][socket.name][socket.cur_level].pop(message);
+  				global.responses[socket.urlRoom][socket.name_obj][socket.cur_round+'_'+socket.cur_level].pop(message);
   			
   			console.log('Recieved messgae from client! '+operation);
-  			console.log(global.responses[socket.urlRoom]);
   		})
 
-  		//When other client prresses next level than the current client, the current client has to init it's level
+  		//When one client gets correct next level than the other client, the other client has to init it's level of same round
   		socket.on('LevelInit',function(level){
+        console.log('Level Increase of '+socket.name+' '+level)
   			socket.cur_level=level;
-  			global.responses[socket.urlRoom][socket.name][socket.cur_level]=[];
-  			console.log(global.responses);
+  			global.responses[socket.urlRoom][socket.name_obj][socket.cur_round+'_'+socket.cur_level]=[];
+        console.log('Next level ->'+ socket.cur_round+'_'+socket.cur_level);
+        socket.time_creations['Round_'+socket.cur_round].push(moment())
+        global.create_time[socket.urlRoom][socket.name_obj] = socket.time_creations
+
 
   		});
 
+      //When one client proceeds to next round than the other client first, the other client has to init it's roundand level of same round
+      socket.on('RoundInit',function(round){
+        console.log('Round Increase of '+socket.name+' '+round)
+        socket.cur_round=round;
+        socket.cur_level = 1;
+        global.responses[socket.urlRoom][socket.name_obj][socket.cur_round+'_'+socket.cur_level]=[];
+        console.log('Next Round ->'+ socket.cur_round+'_'+socket.cur_level);
+        socket.time_creations['Round_'+socket.cur_round] = []
+        socket.time_answers['Round_'+socket.cur_round] = []
+        socket.time_creations['Round_'+socket.cur_round].push(moment())
+        global.create_time[socket.urlRoom][socket.name_obj] = socket.time_creations
+
+
+      });
+
   		//When Client press next level button
-  		socket.on('ProceedToNextlevel',function(){
-  			console.log('Recieved messgae from client! to proceed to next level');
-  			var other_player;
-  			if(socket.name =='O_1')
-  				other_player = 'O_2';
-  			else
-  				other_player = 'O_1';
-  			var numClients = io.sockets.clients(socket.urlRoom).length;
-  			
-  			if(numClients==1)
-  				socket.emit('Recieve','no player')
+  		socket.on('ProceedToNextlevel_Round',function(){
+  			console.log('Recieved messgae from client to proceed to next Round/level');
+        var numClients = io.sockets.clients(socket.urlRoom).length;
+        if(numClients==1)
+          socket.emit('Recieve','no player')
 
-  			else{
-  				if(global.responses[socket.urlRoom][socket.name][socket.cur_level].equals(global.responses[socket.urlRoom][other_player][socket.cur_level])){
+        else {
+      			
+            other_player = findOtherPlayer(socket)
 
-	  				if((socket.cur_level+1)<=socket.levels){
-			  			socket.cur_level+=1
-			  			global.responses[socket.urlRoom][socket.name][socket.cur_level]=[];
-			  			socket.emit('RecieveLevel',socket.cur_level);
-			  			socket.broadcast.to(socket.urlRoom).emit('Recieve','next');
-			  			socket.broadcast.to(socket.urlRoom).emit('RecieveLevel',socket.cur_level);	
-		  			}
-		  			else{
-		  				socket.emit('Recieve','finish');
-			  			socket.broadcast.to(socket.urlRoom).emit('Recieve','fin_next');
-		  			}
-	  			}
+      			if(global.responses[socket.urlRoom][socket.name_obj][socket.cur_round+'_'+socket.cur_level].equals(global.responses[socket.urlRoom][other_player][socket.cur_round+'_'+socket.cur_level])){
 
-	  			else{
-		  			console.log('No match yet!')
-		  			socket.emit('Recieve','no match')
-	  				}
-	  	   }
+                //For incrementing to next level of same round 
+                if(socket.cur_level<3){
+      
+                  socket.cur_level+=1;
+                  socket.time_creations['Round_'+socket.cur_round].push(moment())
+                  global.create_time[socket.urlRoom][socket.name_obj] = socket.time_creations
+                  console.log('Level Increase of player'+socket.name_obj+'_'+socket.cur_level);
+                  global.responses[socket.urlRoom][socket.name_obj][socket.cur_round+'_'+socket.cur_level]=[];
+                  socket.emit('RecieveLevel_Round',socket.cur_level,socket.cur_round,2);
+                  socket.broadcast.to(socket.urlRoom).emit('RecieveLevel_Round',socket.cur_level,socket.cur_round,2);  
+                  socket.broadcast.to(socket.urlRoom).emit('Recieve','next_level');
+                }
+
+                //For incrementing to next round!
+                else{
+      	  				
+                    var other_player = findOtherPlayer(socket)
+                    var time_c_1 = global.create_time[socket.urlRoom][other_player]
+                    var time_a_1 = global.answer_time[socket.urlRoom][other_player]
+
+                    total_time_player_1 =0
+                    for(i=0;i<3;i++){
+                      
+                      time = socket.time_creations['Round_'+socket.cur_round][i].diff(socket.time_answers['Round_'+socket.cur_round][i],'seconds')
+                      
+                      if(time < 0)
+                        time*=-1
+                      
+                      total_time_player_1 +=time
+                    
+                    }
+
+                    total_time_player_2 =0
+                    for(i=0;i<3;i++){
+                      
+                      time = time_c_1['Round_'+socket.cur_round][i].diff(time_a_1['Round_'+socket.cur_round][i],'seconds')
+                      
+                      if(time < 0)
+                        time*=-1
+                      
+                      total_time_player_2 +=time
+                    
+                    }
+
+                  console.log('player1_time in Round_->',total_time_player_1)
+                  console.log('player2_time->',total_time_player_2)
+
+                  var flag_win = 0
+                  if(total_time_player_1 < total_time_player_2){
+                    console.log(global.round_wins)
+                    console.log(global.round_wins[socket.urlRoom][other_player])
+                    global.round_wins[socket.urlRoom][socket.name_obj].push(1)
+                    global.round_wins_points[socket.urlRoom][socket.name_obj]+=2
+                    global.round_wins[socket.urlRoom][other_player].push(0)
+                    flag_win = 1
+                    global.round_win_number[socket.urlRoom][socket.name_obj]+=1
+                 }
+                 else{
+                      console.log(global.round_wins)
+                      console.log(global.round_wins[socket.urlRoom][other_player])
+
+                      global.round_wins[socket.urlRoom][other_player].push(1)
+                      global.round_wins_points[socket.urlRoom][other_player]+=2
+                      global.round_wins[socket.urlRoom][socket.name_obj].push(0)
+
+                      global.round_win_number[socket.urlRoom][other_player]+=1
+
+                 }
+
+                  p1_win = global.round_win_number[socket.urlRoom][socket.name_obj]
+
+                  console.log(socket.name+'->'+total_time_player_1) 
+                  
+                  p2_win = global.round_win_number[socket.urlRoom][other_player] 
+
+                  if((socket.cur_round+1)<=socket.rounds && (p1_win<3 && p2_win<3 )){  
+
+                    socket.cur_level=1
+                    socket.cur_round+=1
+                    socket.time_creations['Round_'+socket.cur_round] = []
+                    socket.time_answers['Round_'+socket.cur_round] = []
+                    socket.time_creations['Round_'+socket.cur_round].push(moment())
+                    global.create_time[socket.urlRoom][socket.name_obj] = socket.time_creations
+
+                    console.log('Round Increase of player'+socket.name_obj+'_'+socket.cur_round);
+      			  			global.responses[socket.urlRoom][socket.name_obj][socket.cur_round+'_'+socket.cur_level]=[];
+      			  			socket.emit('RecieveLevel_Round',socket.cur_level,socket.cur_round,flag_win);
+
+                    socket.broadcast.to(socket.urlRoom).emit('RecieveLevel_Round',socket.cur_level,socket.cur_round,!flag_win); 
+      			  			socket.broadcast.to(socket.urlRoom).emit('Recieve','next_round');
+      		  			}
+      		  			else{
+                    flag_round = 0
+                    if(p1_win>p2_win){
+                      flag_round = 1
+                      global.round_wins_points[socket.urlRoom][socket.name_obj]+=10
+                    }
+                    else{
+                      global.round_wins_points[socket.urlRoom][other_player]+=10
+                    }
+                   
+                   	socket.emit('finish',flag_win,flag_round);
+      			  			socket.broadcast.to(socket.urlRoom).emit('finish',!flag_win,!flag_round);
+      		  			}
+
+              }
+    	  			
+              }
+
+    	  			else{
+    		  			console.log('No match yet!')
+    		  			socket.emit('Recieve','no match')
+    	  				}
+           }
   			
   		})
    
--- static/js/engine.js
+++ static/js/engine.js
@@ -1,19 +1,25 @@
 
-function join_room(name){
-    room_vars.$startRoom(name);
+function join_room(Roomname,uname){
+    room_vars.$room_static = Roomname;
+    room_vars.$startRoom(Roomname,uname);
     room = room_vars.$room;
     room_vars.$finish = 0
-}
+    room_vars.$round_wins =0; 
+    room_vars.$name = uname;
+}  
 
 var room_vars = new Room_vars();
 var url = 'http://localhost:7000/';
 
 function Room_vars() {
   this.$socket;
+  this.$room_static;
   this.$room;
   this.$name;
   this.$cur_level;
   this.$finish;
+  this.$cur_round;
+  this.round_wins ;
 }
 
 Room_vars.prototype.$connection = function(url, opt) {
@@ -21,19 +27,19 @@ Room_vars.prototype.$connection = function(url, opt) {
   this.$socket = io.connect(url, opt);
 };
 
-Room_vars.prototype.$startRoom = function($room) {    
+Room_vars.prototype.$startRoom = function($room,$uname) {    
     this.$room = $room;
-    this.$socket.emit('create or join', this.$room);
+    this.$socket.emit('create or join', this.$room,$uname);
 };
 
 room_vars.$connection(url);
 var socket = room_vars.$socket;
 
 //When Room is created first time
-socket.on('created', function(room,name) {
+socket.on('created', function(room) {
   console.log('Created game-> ' + room);
-  room_vars.$name = name;
   room_vars.$cur_level =1;
+  room_vars.$cur_round = 1;
   console.log('You have joined game-> ' + room);
 });
 
@@ -45,9 +51,9 @@ socket.on('join', function(room) {
 });
 
 //Game Room joined event 
-socket.on('joined', function(room,name) {
-	room_vars.$name = name;
+socket.on('joined', function(room) {
 	room_vars.$cur_level =1;
+  room_vars.$cur_round = 1;
   	console.log('You have joined game-> ' + room);
 });
 
@@ -66,24 +72,20 @@ socket.on('Recieve', function(response) {
 		alert('Some Internal Error occurred,Pls try again later')
 		document.location=url
 	}
-	if(response=='next'){
-		alert('Other player pressed proceed to next level and yours and other players checked image(s) matched,proceding to next level ')
-		socket.emit('LevelInit',room_vars.$cur_level+1);
+	if(response=='next_round'){
+		alert('Proceding to next round ')
+		socket.emit('RoundInit',room_vars.$cur_round);
 	}
+
+  if(response=='next_level'){
+    alert('Other player and your answers matched,proceding to next level of same round')
+    socket.emit('LevelInit',room_vars.$cur_level);
+  }
+
 	if(response=='no match'){
 		alert("Sorry,Can't Proceed, Your Responses don't match with other player yet ")
 	}
-	if(response=='finish'){
-    room_vars.$finish = 1
-		alert('Congrats! you manged to tag all the images correctly with other opponent');
-		document.location = url;
-	}
-
-	if(response=='fin_next'){
-    room_vars.$finish = 1
-		alert('Other player pressed proceed to next level and yours and other players checked image(s) matched,....Congrats! you manged to tag all the images correctly with other opponent' )
-		document.location = url;
-	}
+	
 	if(response=='no player'){
 		alert('Please wait , let other player join the game before moving further')
 	}
@@ -93,20 +95,37 @@ socket.on('Recieve', function(response) {
 	}
 });
 
+socket.on('finish',function(flag_win,flag_round){
+      
+      room_vars.$finish = 1
+      if(flag_round == 1)
+          alert('Congrats You won the GAME!')
+      else
+          alert('You lost the game')
+          //document.location = url;
+  
+})
 //Change In level
-socket.on('RecieveLevel', function(level) {
+socket.on('RecieveLevel_Round', function(level,round,flag) {
   room_vars.$cur_level = level;
-  console.log('Change Of level--->'+ level);
+  room_vars.$cur_round = round
+  console.log('Change Of level/Round--->'+ round+'_'+level);
+  if(flag==0)
+    alert('You Lost the Round')
+  else if (flag==1)
+    alert('You Won the round!')
+  else
+    console.log(' ')
 
-  $('#level_text').html('Welcome To '+room_vars.$room+'-Level'+room_vars.$cur_level);
-  $("#base_image").attr("src","/static/images/"+room+"/level_"+room_vars.$cur_level+"/0.jpg");
-  $("#src1").attr("src","/static/images/"+room_vars.$room+"/level_"+room_vars.$cur_level+"/1.jpg");
+  $('#level_text').html(room_vars.$room_static+' On Round->'+round+'& Level'+room_vars.$cur_level);
+  $("#base_image").attr("src","/static/images/"+room_vars.$room_static+"/round_"+room_vars.$cur_round+"/"+room_vars.$cur_level+"_0.jpg");
+  $("#src1").attr("src","/static/images/"+room_vars.$room_static+"/round_"+room_vars.$cur_round+"/"+room_vars.$cur_level+"_1.jpg");
   $('#o1').attr('checked', false); 
-  $("#src2").attr("src","/static/images/"+room_vars.$room+"/level_"+room_vars.$cur_level+"/2.jpg");
+  $("#src2").attr("src","/static/images/"+room_vars.$room_static+"/round_"+room_vars.$cur_round+"/"+room_vars.$cur_level+"_2.jpg");
   $('#o2').attr('checked', false); 
-  $("#src3").attr("src","/static/images/"+room_vars.$room+"/level_"+room_vars.$cur_level+"/3.jpg");
+  $("#src3").attr("src","/static/images/"+room_vars.$room_static+"/round_"+room_vars.$cur_round+"/"+room_vars.$cur_level+"_3.jpg");
   $('#o3').attr('checked', false); 
-  $("#src4").attr("src","/static/images/"+room_vars.$room+"/level_"+room_vars.$cur_level+"/4.jpg");
+  $("#src4").attr("src","/static/images/"+room_vars.$room_static+"/round_"+room_vars.$cur_round+"/"+room_vars.$cur_level+"_4.jpg");
   $('#o4').attr('checked', false); 
 });
 
@@ -155,7 +174,7 @@ $('#o4').change(function() {
 $('#next').click(function(){
 
 	if(checkbox_check())
-		socket.emit('ProceedToNextlevel')
+		socket.emit('ProceedToNextlevel_Round')
 	else
 		alert('Please Select atleast 1 option to proceed further!')
 })
